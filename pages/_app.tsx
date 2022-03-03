c529965afe0f849effb9c461928ce00a5a38c97e
chloe41297 <79011228+chloe41297@users.noreply.github.com>
2022-03-03 11:43:58 +0900

    chore: 초기설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,98 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { store } from "redux/store";
+import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { Provider as ReduxProvider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    html {
+        font-size: 62.5%;
+        scroll-behavior: smooth;
+        overflow-x: hidden;
+    }
+    
+    body {
+        font-size: 1.6rem;
+        font-family: 'Inter', 'sans-serif';
+        user-select: none;
+        -webkit-user-select: none; 
+        -moz-user-select: none;   
+        -ms-user-select: none;     
+        -o-user-select: none;
+    }
+    
+    a {
+        text-decoration: none;
+        color:black;
+    }
+    ul {
+        list-style-type: none;
+    }
+    img{
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      -khtml-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+    button{
+      border:none;
+      cursor:pointer;
+      
+      &:focus{
+        outline:none;
+      }
+    }
+    input {
+      outline:none;
+      padding: 0 1.5rem;
+      &:focus::placeholder{
+        color:transparent;
+      }
+    }
+    .flex-center {
+      display:flex;
+      align-items:center;
+      justify-content: center;
+    }
+    .flex-center-C {
+      display:flex;
+      align-items:center;
+      justify-content: center;
+      flex-direction:column;
+    }
+  `;
+
+  const theme = {
+    colors: {
+      //@ common style
+      mainColor: "#FF6363",
+      pointColor: "",
+      lightblue: "#C5E2EE",
+      starColor: "#fd4",
+      grayOne: "#F7F7F7",
+      blackOne: "#1c1c1c",
+      blackTwo: "#111",
+    },
+  };
+  const persistor = persistStore(store);
+  return (
+    <ReduxProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </PersistGate>
+    </ReduxProvider>
+  );
 }
-
-export default MyApp
---
+++ redux/slice.ts
@@ -0,0 +1,33 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+import type { AppState } from "./store";
+
+export interface FormState {
+  value: number;
+  status: "idle" | "loading" | "failed";
+}
+
+const initialState: FormState = {
+  value: 0,
+  status: "idle",
+};
+
+export const formSlice = createSlice({
+  name: "form",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = formSlice.actions;
+export const selectForm = (state: AppState) => state.form;
+export default formSlice.reducer;
