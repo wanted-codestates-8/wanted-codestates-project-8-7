90fb4facc6ec9d3dcaf4bff8b42ed76b61e54cb2
chloe41297 <79011228+chloe41297@users.noreply.github.com>
2022-03-03 20:46:51 +0900

    feat:forms page 레이아웃 구성

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -52,43 +52,76 @@ const Forms: NextPage = () => {
 
   return (
     <Main>
-      <TitleSection></TitleSection>
+      <InputForm>
+        <Section>
+          <Title>제목*</Title>
+          <Input></Input>
+        </Section>
 
-      <FormSection>
-        <FormList>
-          {/* map으로 돌아갈것임 */}
-          <FormItem>
-            <Form
-              state={{
-                key: "0",
-                id: "0",
-                type: "text",
-                required: true,
-                label: "이름",
-              }}
-              onChange={onChange}
-            />
-          </FormItem>
-        </FormList>
-      </FormSection>
+        <Section>
+          <Title>필드목록*</Title>
+          <FormList>
+            {/* map으로 돌아갈것임 */}
+            <FormItem>
+              <Form
+                state={{
+                  key: "0",
+                  id: "0",
+                  type: "text",
+                  required: true,
+                  label: "이름",
+                }}
+                onChange={onChange}
+              />
+            </FormItem>
+          </FormList>
+        </Section>
 
-      <AddButton />
+        <AddButton>필드 추가하기</AddButton>
 
-      <SaveSection />
+        <SaveButton>저장 하기</SaveButton>
+      </InputForm>
     </Main>
   );
 
   //   return data.formList.map((data,idx) => <Form key={idx} state={} onChange={}/>);
 };
-
-const TitleSection = styled.section``;
-const FormSection = styled.section``;
+const InputForm = styled.form`
+  width: 80%;
+  margin: 0 auto;
+`;
+const Title = styled.h3`
+  color: grey;
+  margin-top: 20px;
+`;
+const Section = styled.section``;
+const Input = styled.input`
+  width: 100%;
+  height: 45px;
+  border: solid 1px #eeeef0;
+  border-radius: 10px;
+  margin: 0 auto;
+`;
 const FormList = styled.ul``;
 const FormItem = styled.li``;
-const AddButton = styled.button``;
-const SaveSection = styled.section``;
-const ListTitle = styled.h3`
-  color: gray;
+const AddButton = styled.button`
+  border: solid 1px blue;
+  color: blue;
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  border-radius: 10px;
+  font-size: 18px;
+`;
+const SaveButton = styled.button`
+  width: 90px;
+  height: 40px;
+  border-radius: 10px;
+  background-color: blue;
+  color: white;
+  float: right;
+  margin-top: 20px;
+  font-size: 18px;
 `;
 
 export default Forms;
