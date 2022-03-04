7fb04f952ee92066129eea6c336b68bb58774ed4
hatoba29 <hatoba29@naver.com>
2022-03-04 23:05:22 +0900

    Fix: 자잘한 문제 수정
    - findBlank.ts: 플레이스홀더, 설명이 사라지던 문제 수정
    - SingleFormList.tsx: 아이템이 적을 때 각 아이템의 크기가 너무 큰 문제 수정
    - dataList/[id]/[i].tsx: 입력된 데이터 목록에서 이미지가 제대로 보이도록 수정

--- utils/findBlank.ts
+++ utils/findBlank.ts
@@ -2,14 +2,15 @@ import { State } from "pages/forms";
 
 export const findBlank = (formList: State[]) => {
   const targets = formList.map((form) => {
-    delete form.description;
-    delete form.placeholder;
-    return Object.values(form);
+    const tempForm = { ...form };
+    delete tempForm.description;
+    delete tempForm.placeholder;
+    return Object.values(tempForm);
   });
 
   for (let target of targets) {
     for (let e of target) {
-      if (e === "") return "true";
+      if (e === "") return true;
       if (Array.isArray(e) && !e.length) return true;
     }
   }
